84dadb5ff4faa283ff5d06c9483bdfb17b6fba0e
djakubowicz <45103701+djakubowicz@users.noreply.github.com>
2025-04-02 18:04:48 +0200

    Update main.tf

--- Terraform/AAP/main.tf
+++ Terraform/AAP/main.tf
@@ -25,7 +25,7 @@ resource "aap_inventory" "my_inventory" {
 resource "aap_host" "create_host" {
   inventory_id = aap_inventory.my_inventory.id
   name = "192.169.101.101"
-#name         = aws_instance/example.public_ip
+#name         = aws_instance.example.public_ip
  }
 
 
