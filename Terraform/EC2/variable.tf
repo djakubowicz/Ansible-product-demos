930e7df8a9077c013272fc9d02b437b8b472ba09
djakubowicz <45103701+djakubowicz@users.noreply.github.com>
2025-04-02 12:04:42 +0200

    Create variable.tf

---
+++ Terraform/EC2/variable.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "AMI ID"
+  default = "ami-0c15e602d3d6c6c4a"
+}
+
+variable "subnet_prv1" {
+  description = "Private Subnet 1"
+  default = "subnet-0e39ff451e3d0d3eb"
+}
+
+variable "access" {
+  description = "Access Key"
+  default = [PUT ACCESS KEY HERE]
+}
+
+variable "secret" {
+  description = "Secret Key"
+  default = [PUT SECRET HERE]
+}
